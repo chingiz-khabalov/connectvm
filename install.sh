03ed034d583f20c9e8b3594b87c494285f5ffcd0
chingiz.khabalov <91259009+chingiz-khabalov@users.noreply.github.com>
2025-02-10 20:38:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp ./main.py /usr/local/bin/connectvm
+mkdir -p ~/.local/bin
+cp ./main.py ~/.local/bin/connectvm
 mkdir -p ~/.config/connectvm
 cp ./bash_completion ~/.config/connectvm/completion
 chmod 744 ~/.config/connectvm/completion

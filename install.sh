26e4bfb8cdc262ba8f25fafcb2ad1e63ee96b086
chingiz-khabalov <chingiz.khabalov123@gmail.com>
2025-06-08 00:48:25 +0800

    changing path installation to .local/bin

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Installing connectvm"
 
 echo "Copying files"
 
-cp ./main.py /usr/local/bin/connectvm
+cp ./main.py ~/.local/bin/connectvm
 mkdir -p ~/.config/connectvm
 cp ./bash_completion ~/.config/connectvm/completion
 chmod 744 ~/.config/connectvm/completion
@@ -13,6 +13,9 @@ echo "OK"
 
 echo "Adding source command to ~/.bashrc file"
 echo "source ~/.config/connectvm/completion" >> ~/.bashrc
+echo "export PATH=~/.local/bin/:$PATH" >> ~/.bashrc
+source "$HOME/.bashrc"
+
 echo "OK"
 
 echo "Installing is complete"
--- uninstall.sh
+++ uninstall.sh
@@ -9,7 +9,8 @@ rm -r ~/.config/connectvm
 echo "OK"
 
 echo "removing sourcing from bashrc"
-sed -i '/source ~/.config/connectvm/completion/d' ~/.bashrc
+sed -i "/source ~\/.config\/connectvm\/completion/d" ~/.bashrc
+sed -i "export PATH=~\/.local\/bin\/:$PATH/d" ~/.bashrc
 echo "OK"
 
 echo "connectvm with its config files has been removed from your system, don't forget to delete this installation directory"

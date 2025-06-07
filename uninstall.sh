49f8b88fff3dc90cc7cc0c50dc96f04305ec06c6
chingiz-khabalov <chingiz.khabalov123@gmail.com>
2025-06-08 00:38:33 +0800

    add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+echo "removing script"
+rm /usr/local/bin/connectvm
+echo "OK"
+
+echo "removing config folder"
+rm -r ~/.config/connectvm
+echo "OK"
+
+echo "removing sourcing from bashrc"
+sed -i '/source ~/.config/connectvm/completion/d' ~/.bashrc
+echo "OK"
+
+echo "connectvm with its config files has been removed from your system, don't forget to delete this installation directory"
